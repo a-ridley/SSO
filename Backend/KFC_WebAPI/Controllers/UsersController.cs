3c261b79bf62d1f88e06607ad21a23dde3e2b7f8
Andrew Soth <Andrew.Soths@gmail.com>
2019-04-10 00:25:50 -0700

    Added route to receive delete requests from individual apps. Needed to add signature validation methods within TokenService.

--- Backend/KFC_WebAPI/Controllers/UsersController.cs
+++ Backend/KFC_WebAPI/Controllers/UsersController.cs
@@ -14,6 +14,7 @@ using ServiceLayer.Services;
 using ManagerLayer.UserManagement;
 using DataAccessLayer.Requests;
 using System.Data.Entity.Infrastructure;
+using System.Net.Http;
 
 namespace KFC_WebAPI.Controllers
 {
@@ -49,6 +50,28 @@ namespace KFC_WebAPI.Controllers
     {
         [Required]
         public string token { get; set; }
+    }
+
+    public class UserDeleteRequestFromApp
+    {
+        // Included in signature
+        public string appId { get; set; }
+        public Guid ssoUserId { get; set; }
+        public string email { get; set; }
+        public long timestamp { get; set; }
+
+        // Excluded from signature
+        public string signature { get; set; }
+
+        // Generate string to be signed
+        public string PreSignatureString()
+        {
+            string acc = "";
+            acc += "ssoUserId=" + ssoUserId + ";";
+            acc += "email=" + email + ";";
+            acc += "timestamp=" + timestamp + ";";
+            return acc;
+        }
     }
 
     public class UsersController : ApiController
@@ -240,6 +263,29 @@ namespace KFC_WebAPI.Controllers
                     return Ok();
                 }
                 return Content(HttpStatusCode.BadRequest, "Service Unavailable");
+            }
+        }
+
+        [HttpPost]
+        [Route("api/users/appdeleteuser")]
+        public async Task<IHttpActionResult> Delete([FromBody] UserDeleteRequestFromApp request)
+        {
+            using (var _db = new DatabaseContext())
+            {
+                HttpClient client = new HttpClient();
+                ITokenService _tokenService = new TokenService();
+                Application app = ApplicationService.GetApplication(_db,Guid.Parse(request.appId));
+                if (!(_tokenService.GenerateSignature(request.PreSignatureString(), app) == request.signature))
+                {
+                    return Content(HttpStatusCode.Unauthorized, "Signature not valid!");
+                }
+                UserManager um = new UserManager();
+                User deletedUser = await um.DeleteUser(_db, request.ssoUserId);
+                if(deletedUser != null)
+                {
+                    return Ok(deletedUser);
+                }
+                return Content(HttpStatusCode.BadRequest, "Delete Failed");
             }
         }
 
