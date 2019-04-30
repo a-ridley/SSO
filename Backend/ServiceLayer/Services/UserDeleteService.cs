3893ae716a461f90e017de33821247e2addd3f80
Andrew Soth <Andrew.Soths@gmail.com>
2019-04-30 09:38:13 -0700

    Removed unnecessary payload class in userdeleteservice

--- Backend/ServiceLayer/Services/UserDeleteService.cs
+++ Backend/ServiceLayer/Services/UserDeleteService.cs
@@ -13,34 +13,12 @@ using System.Security.Cryptography;
 
 namespace ServiceLayer.Services
 {
-    public class UserDeletePayload
-    {
-        // Included in signature
-        public Guid ssoUserId { get; set; }
-        public string email { get; set; }
-        public long timestamp { get; set; }
-
-        // Excluded from signature
-        public string signature { get; set; }
-
-        // Generate string to be signed
-        public string PreSignatureString()
-        {
-            string acc = "";
-            acc += "ssoUserId=" + ssoUserId + ";";
-            acc += "email=" + email + ";";
-            acc += "timestamp=" + timestamp + ";";
-            return acc;
-        }
-    }
-
-
     public class UserDeleteService
     {
         public async Task<HttpResponseMessage> SendDeleteRequest(string deleteURL, Dictionary<string, string> payload)
         {
             HttpClient client = new HttpClient();
-
+            
             var stringPayload = JsonConvert.SerializeObject(payload);
             var jsonPayload = new StringContent(stringPayload, Encoding.UTF8, "application/json");
             var request = await client.PostAsync(deleteURL, jsonPayload);
