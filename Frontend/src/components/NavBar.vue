44fd22b6bfc7b408540a58c4605a0fc78a906d38
Alfredo Vargas <vargas.alfredojr@gmail.com>
2019-05-01 03:44:34 -0700

    added new logo

--- Frontend/src/components/NavBar.vue
+++ Frontend/src/components/NavBar.vue
@@ -1,45 +1,57 @@
 <template>
   <v-toolbar app>
+      <v-img :src="image" id="logo"></v-img>
     <v-toolbar-title class="headline text-uppercase">
-      <span>KFC SSO</span>
+      <span>SPG SSO</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <div>
+    <v-toolbar-items class="hidden-sm-and-down" >
       <v-btn v-if="!isLoggedIn.isLogin" to="/home" flat>Home</v-btn>
       <v-btn v-else to="/dashboard" flat>Home</v-btn>
-    </div>
-    <v-btn to="/register" flat v-if="!isLoggedIn.isLogin">Register</v-btn>
-    <v-btn to="/about" flat>About</v-btn>
-    <v-menu offset-y id="appDropDown">
-      <template slot="activator">
-        <v-btn flat>
-          <span>Application</span>
-          <v-icon>expand_more</v-icon>
-        </v-btn>
-      </template>
-      <v-list dense>
-        <v-list-tile v-for="link in appLinks"
-                      :key="link.text"
-                      router :to="link.route" >
-          <v-list-tile-title>{{link.text}}</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
+      <v-btn to="/register" flat v-if="!isLoggedIn.isLogin">Register</v-btn>
+      <v-btn to="/about" flat>About</v-btn>
+      <v-menu offset-y id="appDropDown">
+        <template slot="activator">
+          <v-btn flat>
+            <span>Application</span>
+            <v-icon>expand_more</v-icon>
+          </v-btn>
+        </template>
+        <v-list dense>
+          <v-list-tile v-for="link in appLinks"
+                        :key="link.text"
+                        router :to="link.route" >
+            <v-list-tile-title>{{link.text}}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+    <v-menu class="hidden-md-and-up">
+      <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+        <v-list dense>
+          <v-list-tile v-for="link in expandMenu"
+                        :key="link.text"
+                        router :to="link.route"
+                        v-if="link.display" >
+            <v-list-tile-title >{{link.text}}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
     </v-menu>
     <div>
       <v-btn to="login" flat v-if="!isLoggedIn.isLogin">Login</v-btn>
       <v-menu offset-y
               content-class="dropdown-menu"
               transition="slide-y-transition" v-if="isLoggedIn.isLogin">
-        <v-btn slot="activator" fab dark color="teal">
+        <v-btn id="avatar" slot="activator" fab dark color="teal">
           <v-avatar dark>
             <span class="white--text headline">{{isLoggedIn.email[0]}}</span>
           </v-avatar>
         </v-btn>
         <v-list dense>
-          <v-list-tile v-for="item in this.UserMenuItems"
-                        :key="item.title"
-                        route :to="item.route"
-                        >
+          <v-list-tile 
+            v-for="item in this.UserMenuItems"
+            :key="item.title"
+            route :to="item.route">
             <v-list-tile-title>{{item.title}}</v-list-tile-title>
           </v-list-tile>
         </v-list>
@@ -54,10 +66,19 @@
     name: 'NavBar',
     data() {
       return {
+        image: require("@/assets/SPG_SSO_Logo.png"),
         appLinks: [
-            { text: 'Register', route: '/add' },
-            { text: 'Generate Key', route: '/key' },
-            { text: 'Delete', route: '/delete' },
+            { text: 'Register', route: '/add', display: true },
+            { text: 'Generate Key', route: '/key', display: true },
+            { text: 'Delete', route: '/delete', display: true },
+        ],
+        expandMenu: [
+          { text: 'Home', route: '/', display: true},
+          { text: 'About', route: '/about', display: true},
+          { text: 'Register', route: '/register', display: true},
+          { text: 'App Register', route: '/add', display: true },
+          { text: 'App Generate Key', route: '/key', display: true },
+          { text: 'App Delete', route: '/delete', display: true },
         ],
         links: [],
         UserMenuItems: [
@@ -75,3 +96,14 @@
     }
   }
 </script>
+
+<style>
+#logo {
+  max-width: 41px
+}
+
+#avatar {
+  height: 50px;
+  width: 50px
+}
+</style>
